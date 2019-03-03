c76481eb9c15f337879cfc097690a56f0610df3b
buyuanlee <buyuanlee@gmail.com>
2019-03-04 00:49:35 +0800

    icon.vue

---
+++ src/components/icon.vue
@@ -0,0 +1,41 @@
+<template>
+    <svg class="aloe-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+    import '../assets/svg'
+
+    export default {
+        name: "aloe-icon",
+        props: {
+            name: {
+                type: String
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .aloe-icon {
+        width: 1em;
+        height: 1em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+
+    @keyframes circle {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    .loading {
+        animation: circle 2s linear infinite;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Icon from './components/icon'
+
+Vue.component('aloe-icon', Icon);
+
 Vue.config.productionTip = false
 
 new Vue({
