de8306db860c5edd8f5e5b6a18bdb0af873df762
buyuanlee <buyuanlee@gmail.com>
2019-03-16 10:12:24 +0800

    Icon

---
+++ docs/.vuepress/components/IconDemos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <aloe-icon name="down"></aloe-icon>
+        <aloe-icon name="up"></aloe-icon>
+        <aloe-icon name="left"></aloe-icon>
+        <aloe-icon name="right"></aloe-icon>
+        <aloe-icon name="download"></aloe-icon>
+        <aloe-icon name="warning"></aloe-icon>
+        <aloe-icon name="info"></aloe-icon>
+        <aloe-icon name="good"></aloe-icon>
+        <aloe-icon name="setting"></aloe-icon>
+        <aloe-icon name="loading" loading></aloe-icon>
+    </div>
+</template>
+
+<script>
+    import Icon from '../../../src/components/icon'
+
+    export default {
+        name: "IconDemos",
+        components: {
+            'aloe-icon': Icon
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    div {
+        padding: 10px 0;
+        display: flex;
+        justify-content: center;
+        font-size: 40px;
+        color:#3eaf7c ;
+        box-shadow: 2px 2px 5px 2px rgba(0,0,0,.2);
+    }
+    .aloe-icon{
+        margin: 15px;
+    }
+</style>
--- docs/components/icon.md
+++ docs/components/icon.md
@@ -2,3 +2,31 @@
 title:Icon-图标
 ---
 # Icon-图标
+AloeUI提供部分常用图标
+
+**实例展示**
+<ClientOnly>
+<IconDemos></IconDemos>
+</ClientOnly>
+
+**示例代码**
+```html
+<aloe-icon name="down"></aloe-icon>
+<aloe-icon name="up"></aloe-icon>
+<aloe-icon name="left"></aloe-icon>
+<aloe-icon name="right"></aloe-icon>
+<aloe-icon name="download"></aloe-icon>
+<aloe-icon name="warning"></aloe-icon>
+<aloe-icon name="info"></aloe-icon>
+<aloe-icon name="good"></aloe-icon>
+<aloe-icon name="setting"></aloe-icon>
+<aloe-icon name="loading" loading></aloe-icon>
+```
+**Attributes**
+:::tip
+Icon的`width`、`height`以及`color`继承父元素的相应属性；
+:::
+|  参数   | 说明   | 类型   | 可选参数| 默认参数 |
+|--------|--------|--------|--------|--------|
+|name    | 图标名称| String |   --   |   --   |
+
